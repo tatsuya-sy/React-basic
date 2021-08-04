6529b92df13f7dbb605d47b83d490a1a38f49ce3
tatsuya-sy <1122ttatsuya@gmail.com>
2021-08-04 12:00:09 +0900

    backgroundColorの変更

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,14 +1,14 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
-    styles: {
-        global: {
-            body: {
-                backgroundColor: "gray.800",
-                color: "gray.100"
-            }
-        }
-    }
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "gray.800",
+        color: "gray.100",
+      },
+    },
+  },
 });
 
 export default theme;
